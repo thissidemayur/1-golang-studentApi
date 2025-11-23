2ba65f1387642bc7a70d36517bcafcbedfa167d0
thissidemayur <thissidemayur@gmail.com>
2025-11-23 08:10:42 +0530

    register user data into DB using CreateStudent() method

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -5,16 +5,20 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/thissidemayur/1-golang-studentsApi/internal/storage"
 	"github.com/thissidemayur/1-golang-studentsApi/internal/types"
 	"github.com/thissidemayur/1-golang-studentsApi/internal/utils/response"
 )
 
 
-func New(w http.ResponseWriter, r *http.Request) {
-	// convert request data to struct
+func New(storage storage.Storage) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// convert request data to struct
 	var student types.Student
 
 	// decode json data to struct
@@ -36,11 +40,21 @@ func New(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Welcome to Student Handler API"))
+	// save info in db
+	lastId,err :=storage.CreateStudent(student.Name,student.Email,student.RollNo)
+	if err != nil {
+		response.WriteJsonResponse(w,http.StatusInternalServerError,response.GeneralError(err))
+		return
+	}
+	slog.Info("User created successfully. ",slog.String("userId",fmt.Sprintf("%d \n",lastId)))
 	response.WriteJsonResponse(w,http.StatusCreated,map[string]string{
 		"success":"true",
+		"id":fmt.Sprint(lastId),
 		"message":"Student created successfully",
 	})
+
+	}
+	
 
 }
 
